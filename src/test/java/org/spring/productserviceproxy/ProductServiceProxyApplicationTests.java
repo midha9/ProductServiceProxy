382a845b28144fce4c1493ffacd1f7a055db627a
Rohit Midha <rohitmidha1999@gmail.com>
2025-04-10 21:35:41 +0530

    first commit

---
+++ src/test/java/org/spring/productserviceproxy/ProductServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.productserviceproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
